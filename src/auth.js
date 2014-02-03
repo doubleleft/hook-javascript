9b933588e7bcc6f069e4649238e2390e63e8d0bc
Endel Dreyer <endel.dreyer@gmail.com>
2014-02-03 13:53:03 -0200

    fix posting custom data using client.auth

--- src/auth.js
+++ src/auth.js
@@ -8,9 +8,10 @@
 DL.Auth = function(client, provider) {
   this.client = client;
   this.provider = provider;
-  this.segments = 'auth/' + this.provider + "/CAAD2jhum0ZCsBAD8bPT5ThKE6YnRvllsoKPa5YvyhxzRl2HTLwcZBc3tUuXFrGEqthJM7TeL8wuBtkQBEQGs3QdmbaxkZALragniqYSBTRRRUNVFxaXJN35EwBDPPatYEWgYOR7M5ZBICLmRiXvRel8pcUiBRA6AoUqzPCaeAOpDJNvhA1MufvTIX7PzXnQZD";
+  this.segments = 'auth/' + this.provider;
  };
 
-DL.Auth.prototype.register = function() {
-  return this.client.post(this.segments);
+DL.Auth.prototype.register = function(additionalInfo) {
+  if (typeof(additionalInfo)==="undefined") { additionalInfo = {}; }
+  return this.client.post(this.segments, additionalInfo);
 };
