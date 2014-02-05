8bcf9b1d0a600675b47fa865cfd8dfa07cde3e57
Endel Dreyer <endel.dreyer@gmail.com>
2014-02-05 19:32:07 -0200

    add .editorconfig settings

--- src/wrap/begin.js
+++ src/wrap/begin.js
@@ -1,10 +1,10 @@
 (function(window) {
-	//
-	// compatibility with amd
+  //
+  // compatibility with amd
   // WARNING: i'm fucking weird, please fix me
-	//
-	window.define = function(factory, func) {
-		try{ delete window.define; } catch(e){ window.define = void 0; } // IE
+  //
+  window.define = function(factory, func) {
+    try{ delete window.define; } catch(e){ window.define = void 0; } // IE
 
     if (typeof(factory)==="function") {
       // whenjs
@@ -12,5 +12,5 @@
     } else {
       window[factory] = func();
     }
-	};
-	window.define.amd = {};
+  };
+  window.define.amd = {};
