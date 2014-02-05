1506dbb88225896f5c40ae91d96b3bf749cea815
Endel Dreyer <endel.dreyer@gmail.com>
2014-02-05 19:28:41 -0200

    fix script inclusion via AMD

--- src/wrap/begin.js
+++ src/wrap/begin.js
@@ -1,9 +1,16 @@
 (function(window) {
 	//
-	// compatibility with when.js
+	// compatibility with amd
+  // WARNING: i'm fucking weird, please fix me
 	//
-	window.define = function(factory) {
+	window.define = function(factory, func) {
 		try{ delete window.define; } catch(e){ window.define = void 0; } // IE
-		window.when = factory();
+
+    if (typeof(factory)==="function") {
+      // whenjs
+      window.when = factory();
+    } else {
+      window[factory] = func();
+    }
 	};
 	window.define.amd = {};
