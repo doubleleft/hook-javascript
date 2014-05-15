ad8a53ef87f0fa4a8ac93c6f57ed4772d6f38cb4
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-15 19:44:14 -0300

    feature request: cordova plugin

---
+++ src/plugins/cordova.js
@@ -0,0 +1,9 @@
+DL.Client.prototype.cordova = {
+  /**
+   * Dependency: https://github.com/phonegap-build/PushPlugin
+   */
+  push: {
+    register: function() {},
+    unregister: function() {}
+  }
+};
