9fc0453b75ac3dac51d0c29f7f9200a24fe5045a
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-22 19:09:46 -0300

    fix sending credentials via query string with Channel/SSE.

--- src/channel/sse.js
+++ src/channel/sse.js
@@ -153,25 +153,24 @@ DL.Channel.SSE.prototype.connect = function() {
   return this.publish('connected').then(function(data) {
     that.collection.where('updated_at', '>', data.updated_at);
 
-    var query = that.collection.buildQuery();
-
-    query['X-App-Id'] = that.collection.client.appId;
-    query['X-App-Key'] = that.collection.client.key;
+    var queryString = 'X-App-Id=' + that.collection.client.appId +
+      '&X-App-Key=' + that.collection.client.key;
 
     // Forward user authentication token, if it is set
-    var auth_token = window.localStorage.getItem(query['X-App-Id'] + '-' + DL.Auth.AUTH_TOKEN_KEY);
+    var auth_token = that.collection.client.auth.getToken();
     if (auth_token) {
-      query['X-Auth-Token'] = auth_token;
+      queryString += '&X-Auth-Token=' + auth_token;
     }
 
     // time to wait for retry, after connection closes
+    var query = that.collection.buildQuery();
     query.stream = {
       'refresh': that.options.refresh_timeout || 1,
       'retry': that.options.retry_timeout || 1
     };
 
     that.client_id = data.client_id;
-    that.event_source = new EventSource(that.collection.client.url + that.collection.segments + "?" + JSON.stringify(query), {
+    that.event_source = new EventSource(that.collection.client.url + that.collection.segments + "?" + queryString + "&" + JSON.stringify(query), {
       withCredentials: true
     });
 
