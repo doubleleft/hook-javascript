efc24529ed7e2f2e345f80082d6789138a61504d
Endel Dreyer <endel.dreyer@gmail.com>
2015-03-27 15:32:02 -0300

    update public documentation

---
+++ api.js
@@ -0,0 +1,42 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Backbone.HookCollection",
+        "Backbone.HookModel",
+        "Cordova",
+        "Hook.Auth",
+        "Hook.Channel.SSE",
+        "Hook.Channel.WEBSOCKETS",
+        "Hook.Client",
+        "Hook.Collection",
+        "Hook.Events",
+        "Hook.Iterable",
+        "Hook.KeyValues",
+        "Hook.Pagination",
+        "Hook.Plugin.Cordova.PushNotification",
+        "Hook.PluginManager",
+        "Hook.System",
+        "OAuth"
+    ],
+    "modules": [
+        "Backbone",
+        "Hook",
+        "Hook.Plugin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Backbone",
+            "name": "Backbone"
+        },
+        {
+            "displayName": "Hook",
+            "name": "Hook",
+            "description": "Deals with user registration/authentication"
+        },
+        {
+            "displayName": "Hook.Plugin",
+            "name": "Hook.Plugin"
+        }
+    ]
+} };
+});
